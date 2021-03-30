5d7f6a5e6e0d69167bb2c2f8e7813be7385d7901
Emily Spoldi <emily.l.spoldi@gmail.com>
2021-03-30 17:42:25 -0400

    imported materialize and general setup for app.js

---
+++ src/App.js
@@ -0,0 +1,22 @@
+// React
+import React, { Component } from 'react';
+
+// Materialize
+import M from 'materialize-css';
+
+class App extends Component {
+    state = {}
+
+    componentDidMount() {
+        M.AutoInit();
+    };
+
+    render() {
+        return (
+            <>
+            </>
+        );
+    }
+}
+
+export default App;
