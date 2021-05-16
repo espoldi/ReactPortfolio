f66dccc9b7e7b80ef3bb098744e25784e817215f
Emily Spoldi <emily.l.spoldi@gmail.com>
2021-05-16 11:16:12 -0400

    added better menu for project navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,63 +3,140 @@ import React from "react";
 // Material UI
 import {
     AppBar,
-    Button,
+    Drawer,
+    IconButton,
     List,
     ListItem,
+    ListItemIcon,
     ListItemText,
-    SwipeableDrawer,
     Toolbar,
     Typography
 } from "@material-ui/core";
 import {
+    ChevronLeft,
+    ChevronRight,
     MenuOpen
 } from "@material-ui/icons";
+import {
+    makeStyles,
+    useTheme
+} from '@material-ui/core/styles';
+import clsx from 'clsx';
+
+const drawerWidth = 240;
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+    },
+    appBar: {
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    appBarShift: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+        marginRight: drawerWidth,
+    },
+    title: {
+        flexGrow: 1,
+    },
+    hide: {
+        display: 'none',
+    },
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+    },
+    drawerPaper: {
+        width: drawerWidth,
+    },
+    drawerHeader: {
+        display: 'flex',
+        alignItems: 'center',
+        padding: theme.spacing(0, 1),
+        // necessary for content to be below app bar
+        ...theme.mixins.toolbar,
+        justifyContent: 'flex-start',
+    },
+    content: {
+        flexGrow: 1,
+        padding: theme.spacing(3),
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        marginRight: -drawerWidth,
+    },
+    contentShift: {
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+        marginRight: 0,
+    },
+}));
 
 export default function Header() {
-    const anchor = "right";
-    const [state, setState] = React.useState({
-        top: false,
-        left: false,
-        bottom: false,
-        right: false,
-    });
+    const classes = useStyles();
+    const theme = useTheme();
+    const [open, setOpen] = React.useState(false);
 
-    const toggleDrawer = (anchor, open) => (event) => {
-        if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-            return;
-        }
+    const handleDrawerOpen = () => { setOpen(true); };
 
-        setState({ ...state, [anchor]: open });
-    };
+    const handleDrawerClose = () => { setOpen(false); };
 
     return (
-        <AppBar>
-            <Toolbar>
-                <Typography variant="h6">
-                    Emily Spoldi's Portfolio
-                </Typography>
+        <>
+            <AppBar
+                position="fixed"
+                className={clsx(classes.appBar, { [classes.appBarShift]: open, })}>
+                <Toolbar>
+                    <Typography variant="h6" noWrap className={classes.title}>My Portfolio</Typography>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="end"
+                        onClick={handleDrawerOpen}
+                        className={clsx(open && classes.hide)}>
+                            <MenuOpen />
+                    </IconButton>
+                </Toolbar>
+            </AppBar>
 
-                <Button
-                    aria-label="project jump menu button"
-                    variant="contained"
-                    color="secondary"
-                    startIcon={<MenuOpen />}
-                    onClick={toggleDrawer(anchor, true)}>
-                    Projects
-                </Button>
-
-                <SwipeableDrawer
-                    anchor={anchor}
-                    open={state[anchor]}
-                    onClose={toggleDrawer(anchor, false)}
-                    onOpen={toggleDrawer(anchor, true)}>
-                        <List>
-                            <ListItem button>
-                                <ListItemText>Testing</ListItemText>
-                            </ListItem>
-                        </List>
-                </SwipeableDrawer>
-            </Toolbar>
-        </AppBar>
+            <main
+                className={clsx(classes.content, {
+                    [classes.contentShift]: open,
+                })}
+            >
+            </main>
+            <Drawer
+                className={classes.drawer}
+                variant="persistent"
+                anchor="right"
+                open={open}
+                classes={{
+                    paper: classes.drawerPaper,
+                }}
+            >
+                <div className={classes.drawerHeader}>
+                    <IconButton onClick={handleDrawerClose}>
+                        {theme.direction === 'rtl' ? <ChevronLeft /> : <ChevronRight />}
+                    </IconButton>
+                    <Typography variant="h6">Projects</Typography>
+                </div>
+                <List>
+                    <ListItem button>
+                        <ListItemIcon></ListItemIcon>
+                        <ListItemText></ListItemText>
+                    </ListItem>
+                </List>
+            </Drawer>
+        </>
     );
 }
