8341350b1a5fd18d956233455150913d5206ef8d
Emily Spoldi <emily.l.spoldi@gmail.com>
2021-05-18 15:02:50 -0400

    replaced temporary drawer with navbar menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,169 +1,124 @@
 import React from "react";
 
-// Components
-import Body from "./Body.js";
-
 // Material UI
 import {
     AppBar,
-    Drawer,
+    Button,
+    ButtonGroup,
+    ClickAwayListener,
+    Grow,
     IconButton,
-    List,
-    ListItem,
-    ListItemIcon,
-    ListItemText,
+    MenuItem,
+    MenuList,
+    Paper,
+    Popper,
     Toolbar,
     Typography
 } from "@material-ui/core";
 import {
-    Brightness3,
+    Brightness4,
     Brightness7,
-    ChevronLeft,
-    ChevronRight,
-    MenuOpen
+    ExpandMore
 } from "@material-ui/icons";
 import {
-    makeStyles,
-    useTheme
+    makeStyles
 } from '@material-ui/core/styles';
-import clsx from 'clsx';
 
-const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
     root: {
         display: 'flex',
     },
-    appBar: {
-        transition: theme.transitions.create(['margin', 'width'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-    },
-    appBarShift: {
-        width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(['margin', 'width'], {
-            easing: theme.transitions.easing.easeOut,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-        marginRight: drawerWidth,
-    },
     title: {
         flexGrow: 1,
-    },
-    hide: {
-        display: 'none',
-    },
-    drawer: {
-        width: drawerWidth,
-        flexShrink: 0,
-    },
-    drawerPaper: {
-        width: drawerWidth,
-    },
-    drawerHeader: {
-        display: 'flex',
-        alignItems: 'center',
-        padding: theme.spacing(0, 1),
-        // necessary for content to be below app bar
-        ...theme.mixins.toolbar,
-        justifyContent: 'flex-start',
-    },
-    content: {
-        flexGrow: 1,
-        padding: theme.spacing(3),
-        transition: theme.transitions.create('margin', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-        marginRight: -drawerWidth,
-    },
-    contentShift: {
-        transition: theme.transitions.create('margin', {
-            easing: theme.transitions.easing.easeOut,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-        marginRight: 0,
     },
 }));
 
 export default function NavBar(props) {
-    const icon = !props.theme ? <Brightness7 /> : <Brightness3 />;
+    const icon = !props.theme ? <Brightness7 /> : <Brightness4 />;
     const classes = useStyles();
-    const theme = useTheme();
-    const [open, setOpen] = React.useState(false);
 
-    const handleDrawerOpen = () => {
-        setOpen(true);
+    const [open, setOpen] = React.useState(false);
+    const anchorRef = React.useRef(null);
+
+    const handleToggle = () => {
+        setOpen((prevOpen) => !prevOpen);
     };
 
-    const handleDrawerClose = () => {
+    const handleClose = (event) => {
+        if (anchorRef.current && anchorRef.current.contains(event.target)) {
+            return;
+        }
+
         setOpen(false);
     };
 
+    function handleListKeyDown(event) {
+        if (event.key === 'Tab') {
+            event.preventDefault();
+            setOpen(false);
+        }
+    }
+
+    // return focus to the button when we transitioned from !open -> open
+    const prevOpen = React.useRef(open);
+    React.useEffect(() => {
+        if (prevOpen.current === true && open === false) {
+            anchorRef.current.focus();
+        }
+
+        prevOpen.current = open;
+    }, [open]);
+
     return (
         <div className={classes.root}>
-            <AppBar
-                position="fixed"
-                className={clsx(classes.appBar, {
-                    [classes.appBarShift]: open,
-                })}
-            >
+            <AppBar position="fixed">
                 <Toolbar>
                     <Typography variant="h6" noWrap className={classes.title}>
                         My Portfolio
                     </Typography>
-                    
-              <IconButton
-                edge="end"
-                color="inherit"
-                aria-label="mode"
-                onClick={() => props.changeTheme(!props.theme)}
-              >
-                {icon}
-              </IconButton>
-            
+
+                    <ButtonGroup variant="contained" color="secondary" aria-label="nav menu">
+                        <Button href="#about">About</Button>
+                        <Button href="#contact">Contact</Button>
+                        <Button
+                            ref={anchorRef}
+                            aria-controls={open ? 'menu-list-grow' : undefined}
+                            aria-haspopup="true"
+                            onClick={handleToggle}
+                            endIcon={<ExpandMore />}>
+                            Projects
+                        </Button>
+                    </ButtonGroup>
+
+                    <Popper open={open} anchorEl={anchorRef.current} role={undefined} transition disablePortal>
+                        {({ TransitionProps, placement }) => (
+                            <Grow
+                                {...TransitionProps}
+                                style={{ transformOrigin: placement === 'bottom' ? 'center top' : 'center bottom' }}
+                            >
+                                <Paper>
+                                    <ClickAwayListener onClickAway={handleClose}>
+                                        <MenuList autoFocusItem={open} id="menu-list-grow" onKeyDown={handleListKeyDown}>
+                                            <MenuItem onClick={handleClose}>Profile</MenuItem>
+                                            <MenuItem onClick={handleClose}>My account</MenuItem>
+                                            <MenuItem onClick={handleClose}>Logout</MenuItem>
+                                        </MenuList>
+                                    </ClickAwayListener>
+                                </Paper>
+                            </Grow>
+                        )}
+                    </Popper>
+
                     <IconButton
-                        color="inherit"
-                        aria-label="open drawer"
                         edge="end"
-                        onClick={handleDrawerOpen}
-                        className={clsx(open && classes.hide)}
-                    >
-                        <MenuOpen />
+                        color="inherit"
+                        aria-label="dark mode"
+                        onClick={() => props.changeTheme(!props.theme)}>
+                        {icon}
                     </IconButton>
                 </Toolbar>
             </AppBar>
-
-            <main
-                className={clsx(classes.content, {
-                    [classes.contentShift]: open,
-                })}
-            >
-                <Body />
-            </main>
-
-            <Drawer
-                className={classes.drawer}
-                variant="persistent"
-                anchor="right"
-                open={open}
-                classes={{
-                    paper: classes.drawerPaper,
-                }}
-            >
-                <div className={classes.drawerHeader}>
-                    <IconButton onClick={handleDrawerClose}>
-                        {theme.direction === 'rtl' ? <ChevronLeft /> : <ChevronRight />}
-                    </IconButton>
-                </div>
-                <List>
-                    <ListItem button>
-                        <ListItemIcon></ListItemIcon>
-                        <ListItemText />
-                    </ListItem>
-                </List>
-            </Drawer>
         </div>
     );
 }
