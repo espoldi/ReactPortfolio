96a19ee68af7ef204c45889e6ed9bff42ae4b374
Emily Spoldi <emily.l.spoldi@gmail.com>
2021-05-18 17:43:33 -0400

    added technologies used

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 // Components
 import buttons from "../images/icons/buttons.png";
@@ -6,12 +6,23 @@ import controller from "../images/icons/controller.png";
 import dice from "../images/icons/dice.png";
 import groceries from "../images/icons/groceries.png";
 import sale from "../images/icons/sale.png";
+import bootstrap from "../images/technology/bootstrap.png";
+import css from "../images/technology/css3.png";
+import express from "../images/technology/express.png";
+import github from "../images/technology/github.png";
+import heroku from "../images/technology/heroku.png";
+import javascript from "../images/technology/javascript.png";
+import materialize from "../images/technology/materialize.png";
+import materialui from "../images/technology/materialui.png";
+import mongodb from "../images/technology/mongodb.png";
+import mysql from "../images/technology/mysql.png";
+import nodejs from "../images/technology/nodejs.png";
+import react from "../images/technology/react.png";
+import redux from "../images/technology/Redux.png";
+import vscode from "../images/technology/vscode.png";
 
 // Material UI
 import {
-    Accordion,
-    AccordionSummary,
-    AccordionDetails,
     Avatar,
     Card,
     CardActions,
@@ -21,10 +32,10 @@ import {
     Chip,
     Grid,
     IconButton,
+    Paper,
     Typography
 } from "@material-ui/core";
 import {
-    ExpandMore,
     GitHub,
     LinkedIn
 } from "@material-ui/icons";
@@ -42,10 +53,16 @@ const useStyles = makeStyles((theme) => ({
         display: 'flex',
         alignItems: 'center',
         padding: theme.spacing(0, 1),
-        // necessary for content to be below app bar
         ...theme.mixins.toolbar,
         justifyContent: 'flex-end',
     },
+    paper: {
+        margin: 15,
+    },
+    tech: {
+        height: 200,
+        width: 200,
+    }
 }));
 
 export default function About() {
@@ -53,63 +70,60 @@ export default function About() {
 
     const chipData = [
         { key: 0, icon: `${buttons}`, label: 'DIY Crafts' },
-        { key: 1, icon: `${controller}`, label: 'Video Games' },
-        { key: 2, icon: `${dice}`, label: 'Dungeons and Dragons' },
+        { key: 1, icon: `${controller}`, label: 'Gaming' },
+        { key: 2, icon: `${sale}`, label: 'Shopping' },
         { key: 3, icon: `${groceries}`, label: 'Cooking' },
-        { key: 4, icon: `${sale}`, label: 'Shopping' }
+        { key: 4, icon: `${dice}`, label: 'Dungeons and Dragons' }
     ];
 
-    const [expanded, setExpanded] = useState(false);
-    const handleChange = (panel) => (event, isExpanded) => {
-        setExpanded(isExpanded ? panel : false);
-    };
+    const tech = [
+        { key: 0, img: `${bootstrap}`, title: 'Bootstrap' },
+        { key: 1, img: `${css}`, title: 'CSS3' },
+        { key: 2, img: `${express}`, title: 'Express' },
+        { key: 3, img: `${github}`, title: 'GitHub' },
+        { key: 4, img: `${heroku}`, title: 'heroku' },
+        { key: 5, img: `${javascript}`, title: 'JavaScript' },
+        { key: 6, img: `${materialize}`, title: 'Materialize' },
+        { key: 7, img: `${materialui}`, title: 'Material UI' },
+        { key: 8, img: `${mongodb}`, title: 'MongoDB' },
+        { key: 9, img: `${mysql}`, title: 'MySQL' },
+        { key: 10, img: `${nodejs}`, title: 'NodeJS' },
+        { key: 11, img: `${react}`, title: 'React' },
+        { key: 12, img: `${redux}`, title: 'Redux' },
+        { key: 13, img: `${vscode}`, title: 'Visual Studio Code' },
+    ];
 
     return (
         <>
             <div className={classes.drawerHeader} id="about"></div>
-            <Card>
-                <CardHeader
-                    title="Emily Spoldi"
-                    subheader="Full Stack Web Developer" />
-                <CardMedia
-                    className={classes.media}
-                    image={'https://avatars.githubusercontent.com/u/72423431?v=4'}
-                    title="Emily Spoldi" />
-                <CardActions>
-                    <IconButton aria-label="github link"
-                        href="https://github.com/espoldi">
-                        <GitHub />
-                    </IconButton>
-                    <IconButton aria-label="linkedin link"
-                        href="https://www.linkedin.com/in/emily-spoldi/">
-                        <LinkedIn />
-                    </IconButton>
-                </CardActions>
-                <CardContent>
-                    <Accordion expanded={expanded === 'introduction'} onChange={handleChange('introduction')}>
-                        <AccordionSummary
-                            expandIcon={<ExpandMore />}
-                            aria-controls="introduction-content"
-                            id="introduction-header">
-                            <Typography className={classes.heading}>Introduction</Typography>
-                        </AccordionSummary>
-                        <AccordionDetails>
-                            <Typography>
-                                My name is Emily Spoldi. I am 28 years old and programming has had a heavy influence on my life since I was little. My parents are both software engineers so technical jargon was common at the dinner table. I was the only one in my immediate family who decided to do something other than programming when deciding on college majors... UNTIL NOW!<br />
-                                <br />
-                            I have spent a few years working with different schools and programs using what I have taught myself or remember from my classes at SCSU and high school robotics team. Now, I am well on my way to making the world a better formatted place. Having grown up in an era where technology was evolving quickly has given me the ability to use some systems without much experience.
-                        </Typography>
-                        </AccordionDetails>
-                    </Accordion>
-                    <Accordion expanded={expanded === 'interests'} onChange={handleChange('interests')}>
-                        <AccordionSummary
-                            expandIcon={<ExpandMore />}
-                            aria-controls="interests-content"
-                            id="interests-header">
-                            <Typography className={classes.heading}>Interests</Typography>
-                        </AccordionSummary>
-                        <AccordionDetails>
+            <Grid container>
+                <Grid item xs="12" md="4" align="center">
+                    <Card>
+                        <CardHeader
+                            title="Emily Spoldi"
+                            subheader="Full Stack Web Developer" />
+                        <CardMedia
+                            className={classes.media}
+                            image={'https://avatars.githubusercontent.com/u/72423431?v=4'}
+                            title="Emily Spoldi" />
+                        <CardActions>
+                            <Grid container></Grid>
+                            <IconButton aria-label="github link"
+                                href="https://github.com/espoldi">
+                                <GitHub />
+                            </IconButton>
+                            <IconButton aria-label="linkedin link"
+                                href="https://www.linkedin.com/in/emily-spoldi/">
+                                <LinkedIn />
+                            </IconButton>
+                            <Grid container></Grid>
+                        </CardActions>
+                        <CardContent>
                             <Grid container>
+                                <Grid item xs="12">
+                                    <Typography variant="h6">Interests</Typography>
+                                    <hr />
+                                </Grid>
                                 {chipData.map((data) => {
                                     return (
                                         <Grid item key={data.key} >
@@ -118,10 +132,39 @@ export default function About() {
                                     );
                                 })}
                             </Grid>
-                        </AccordionDetails>
-                    </Accordion>
-                </CardContent>
-            </Card>
+                        </CardContent>
+                    </Card>
+                </Grid>
+                <Grid item xs="12" md="7">
+                    <Paper className={classes.paper}>
+                        <Typography>
+                            My name is Emily Spoldi. I am 28 years old and programming has had a heavy influence on my life since I was little. My parents are both software engineers so technical jargon was common at the dinner table. I was the only one in my immediate family who decided to do something other than programming when deciding on college majors... UNTIL NOW! I have spent a few years working with different schools and programs using what I have taught myself or remember from my classes at SCSU and high school robotics team. Now, I am well on my way to making the world a better formatted place. Having grown up in an era where technology was evolving quickly has given me the ability to use some systems without much experience.
+                        </Typography>
+                    </Paper>
+                </Grid>
+                <Grid item xs="12">
+                    <Paper className={classes.paper}>
+                        <Typography variant="h4">Technologies</Typography>
+                        <hr />
+                        <Grid container>
+                            {tech.map((tile) => {
+                                return (
+                                    <Grid item key={tile.key}>
+                                        <Card>
+                                            <CardContent>
+                                                <img
+                                                    src={tile.img}
+                                                    title={tile.title}
+                                                    className={classes.tech} />
+                                            </CardContent>
+                                        </Card>
+                                    </Grid>
+                                );
+                            })}
+                        </Grid>
+                    </Paper>
+                </Grid>
+            </Grid>
         </>
     );
 }
